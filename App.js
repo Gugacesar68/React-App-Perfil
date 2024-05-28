4935be633805c5c54a6d994b7c1e26932ae8f4a8
Gugacesar68 <gugacesar2000@gmail.com>
2024-05-28 14:00:02 -0300

    Primeira tela construída

--- App.js
+++ App.js
@@ -1,15 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -17,5 +8,29 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    textAlign: 'right'
   },
+
+  imagem_perfil: {
+    aspectRatio: 1/1,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: 'black',
+    borderRadius: 1000
+  }
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.imagem_container}>
+          <Image style={styles.imagem_perfil} source={require('./images/b8abe72dec611e80cc1a5b08bf849460.jpg')}/>
+      </View>
+      <Text>Olá mundo!</Text>
+      <Text>Meu nome é Gustavo César</Text>
+      <Text>Finalmente meu primeiro App React Native está completo</Text>
+      <Text>Mas eu não estou entendendo Nada o que está acontecendo aqui</Text>
+      {/* <StatusBar style="auto" /> */}
+    </View>
+  );
+}
